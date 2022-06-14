755444440ae3075b419cea7f74d2a0b1898064d1
Vanesa Rogero <vanesarogero@gmail.com>
2022-06-14 16:13:33 +0100

    condicionales

---
+++ arrays/ejercicio4.js
@@ -0,0 +1,8 @@
+//Crea un array vacío. Despues pidele al usuario que introduzca 10 valores a ese
+//array Usar un for para mostrarlos en pantalla
+let array= []
+
+for (let i = 0; i <9 array.length; i++) {
+    const element = array[i];
+    
+}
